2ad3d399926ef32f5a62a7be6aefde902bd6ee1a
Tomas Kundis <tomas.kundis@example.com>
2022-10-03 03:04:39 +0200

    FEAT: Set root URL / to WelcomeView.

--- example-for-endevel/src/router/index.ts
+++ example-for-endevel/src/router/index.ts
@@ -5,8 +5,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/welcome',
-      name: 'welcome-screen',
+      path: '/',
+      name: 'welcome-view',
       component: WelcomeView,
     },
   ],
