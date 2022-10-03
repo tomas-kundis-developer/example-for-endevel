50fdd62229dcc44209729f698d1a171d4bb0bacd
Tomas Kundis <tomas.kundis@example.com>
2022-10-03 13:14:04 +0200

    REFACT: Separate welcome view layout and welcome screen ui.

--- example-for-endevel/src/views/WelcomeView.vue
+++ example-for-endevel/src/views/WelcomeView.vue
@@ -1,22 +1,9 @@
 <template>
   <div class="container">
-    <div class="flex flex-col justify-center">
-      <div class="flex flex-col items-center pt-3">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <div class="pt-2">Welcome in Vue application!</div>
-      </div>
-      <div class="mt-5">
-        <nav class="flex flex-col items-center">
-          <div><RouterLink to="/">Home</RouterLink></div>
-          <div><a href="https://vuejs.org/" target="_blank">Vue.js Homepage</a></div>
-        </nav>
-      </div>
-    </div>
+    <WelcomeScreen />
   </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import WelcomeScreen from '@/components/welcome-screen/WelcomeScreen.vue';
 </script>
-
-<style scoped></style>
