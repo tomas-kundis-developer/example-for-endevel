f6c986c1d0608abecc6955904c396264ef4ddfac
Tomas Kundis <tomas.kundis@example.com>
2022-10-03 03:58:50 +0200

    FEAT: Set main layout.

--- example-for-endevel/tailwind.config.js
+++ example-for-endevel/tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      // center container class by default
+      // (you don't have to use class="container mx-auto", but just class="container")
+      center: true,
+      // default horizontal padding for container class
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
