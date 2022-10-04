a7e93bfdf18ffa521145b72ef5975e0267104b7b
Tomas Kundis <tomas.kundis@example.com>
2022-10-04 04:09:26 +0200

    WIP: WelcomeScreen test screen.

--- example-for-endevel/src/components/welcome-screen/WelcomeScreen.vue
+++ example-for-endevel/src/components/welcome-screen/WelcomeScreen.vue
@@ -6,9 +6,19 @@
     </div>
     <div class="mt-5">
       <div>Selected car: {{ selectedCar }}</div>
-      <Select :options="cars" v-model="selectedCar" defaultValue="volvo" />
+      <Select v-model="selectedCar" :options="cars" defaultValue="volvo" />
+      <Select v-model="selectedCar" :options="cars" placeholder="Select a car" />
     </div>
     <div class="mt-5">
+      <div>Selected car 2: {{ selectedCar2 }}</div>
+      <Select2 v-model="selectedCar2" :options="cars2" placeholder="Select a car" />
+      <Select2 v-model="selectedCar2" :options="cars2" defaultValue="volvo" />
+      <Select2 v-model="selectedCar2" :options="cars2" defaultValue="volvo" disabled />
+    </div>
+    <div class="mt-5">
+      <Slider :initialValue="10" :min="5" :max="15" />
+    </div>
+    <div class="mt-10">
       <nav class="flex flex-col items-center">
         <div><RouterLink to="/">Home</RouterLink></div>
         <div><a href="https://vuejs.org/" target="_blank">Vue.js Homepage</a></div>
@@ -18,10 +28,14 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
-import Select from '@/components/ui/select/Select.vue';
-import type { ISelectOption } from '@/components/ui/select/ISelectOption';
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+import Select from '@/components/ui/select/Select.vue';
+import Select2 from '@/components/ui/select2/Select2.vue';
+import Slider from '@/components/ui/Slider.vue';
+import type { ISelectOption } from '@/components/ui/select/ISelectOption';
+import type { ISelectOption2 } from '@/components/ui/select2/ISelectOption2';
 
 const cars: ISelectOption[] = [
   { description: 'Audi', value: 'audi' },
@@ -29,7 +43,14 @@ const cars: ISelectOption[] = [
   { description: 'Volvo', value: 'volvo', disabled: false },
 ];
 
+const cars2: ISelectOption2[] = [
+  { label: 'Audi', value: 'audi' },
+  { label: 'BMW', value: 'bmw', disabled: true },
+  { label: 'Volvo', value: 'volvo' },
+];
+
 const selectedCar = ref('');
+const selectedCar2 = ref('');
 </script>
 
 <style lang="scss" scoped></style>
