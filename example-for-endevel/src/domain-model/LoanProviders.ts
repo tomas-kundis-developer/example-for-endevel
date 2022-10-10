00aaee355a3173687e433babbb91e5bd7990d7a6
Tomas Kundis <tomas.kundis@example.com>
2022-10-10 02:53:14 +0200

    FEAT: Add LoanProviders enum to domain model.

---
+++ example-for-endevel/src/domain-model/LoanProviders.ts
@@ -0,0 +1,10 @@
+export enum ELoanProviders {
+  CSAS = 'Česká spořitelna',
+  CSOB = 'Československá obchodní banka',
+  HYPOTECNI_BANK = 'Hypoteční banka',
+  KB = 'Komerční banka (KB)',
+  MBANK = 'mBank',
+  MONETA_MONEY_BANK = 'MONETA Money Bank',
+  RF_BANK = 'Raiffeisenbank',
+  UNICREDIT_BANK = 'UniCredit Bank',
+}
