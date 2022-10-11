bbd1af3ca6db0534deabc6b7564a6d0131fd45cf
Tomas Kundis <tomas.kundis@example.com>
2022-10-11 22:29:48 +0200

    FEAT: Add comments for select with bank options factory on BankOfferScreen.

--- example-for-endevel/src/components/bank-offers-screen/services/bankSelectOptionsFactory.ts
+++ example-for-endevel/src/components/bank-offers-screen/services/bankSelectOptionsFactory.ts
@@ -3,10 +3,17 @@ import type { ISelectOption2 } from '@/components/ui/select2/ISelectOption2';
 
 export const ALL_BANKS_SELECT_VALUE = 'all';
 
+/**
+ * Create options object for UI select.
+ *
+ * Options will contain all banks specified in {@link ELoanProviders} with extra item 'all' at the beginning
+ * of the select list.
+ * @see ELoanProviders
+ */
 export const bankSelectOptionsFactory = (): ISelectOption2[] => {
   const selectOptions: ISelectOption2[] = [];
 
-  // First entry in select will be 'all' - when selected, show all financial institutions.
+  // First item in select list will be 'all' - when selected, show all financial institutions.
   selectOptions.push({ label: '-- ALL FINANCIAL INSTITUTIONS --', value: ALL_BANKS_SELECT_VALUE });
 
   // Convert known list of all financial institutions to format for select options.
