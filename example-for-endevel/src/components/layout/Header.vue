3f87a832fe521e90315f0684a9f66c9b9004f903
Tomas Kundis <tomas.kundis@example.com>
2022-10-12 17:20:53 +0200

    FEAT: Add Header layout component.

---
+++ example-for-endevel/src/components/layout/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="pt-1">
+    <div class="hover:animate-pulse font-semibold text-brand-link"><RouterLink to="/">&lt;-- Home</RouterLink></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
