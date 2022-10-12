843a59cc941cb67c46dde71f13fab96c31c66de4
Tomas Kundis <tomas.kundis@example.com>
2022-10-12 22:41:13 +0200

    FEAT: Setup vue-loading-overlay as global plugin.

---
+++ example-for-endevel/src/config/vueLoadingConfig.ts
@@ -0,0 +1,12 @@
+/**
+ * vue-loading-overlay spinner plugin customization.
+ *
+ * @see https://openbase.com/js/vue-loading-overlay
+ */
+export const vueLoadingConfig = {
+  canCancel: false,
+  color: '#16a34a',
+  height: 100,
+  width: 100,
+  backgroundColor: '#4b4b4b',
+};
--- example-for-endevel/src/main.ts
+++ example-for-endevel/src/main.ts
@@ -1,6 +1,20 @@
+// Vue
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
+// External libraries and plugins.
+
+// vue-loading-overlay spinner plugin https://openbase.com/js/vue-loading-overlay
+import VueLoading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+// Configuration.
+
+import { vueLoadingConfig } from '@/config/vueLoadingConfig';
+
+// Global static css and dynamic scss.
 
 import './assets/main.css';
 import './scss/global.scss';
@@ -8,5 +22,6 @@ import './scss/global.scss';
 const app = createApp(App);
 
 app.use(router);
+app.use(VueLoading, vueLoadingConfig);
 
 app.mount('#app');
