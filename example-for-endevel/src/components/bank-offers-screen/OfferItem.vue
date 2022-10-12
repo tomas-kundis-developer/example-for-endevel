bf2d654f2ba95b2b13c35efdf2a0965b090264ab
Tomas Kundis <tomas.kundis@example.com>
2022-10-13 01:36:11 +0200

    FIX: Bad class names.

--- example-for-endevel/src/components/bank-offers-screen/OfferItem.vue
+++ example-for-endevel/src/components/bank-offers-screen/OfferItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mt-5 px-2 py-1 border-2 border-brand-green rounded-xl bg-gray-light">
     <div class="flex flex-col gap-4">
-      <div class="flex fle-row justify-between">
+      <div class="flex flex-row justify-between">
         <div class="text-gray">Banka</div>
         <div class="text-lg font-semibold italic text-brand-green">{{ props.offer.bankName }}</div>
       </div>
-      <div class="flex fle-row justify-between">
+      <div class="flex flex-row justify-between">
         <div class="font-semibold text-gray">Oficiální úroková sazba</div>
         <div class="font-bold">{{ props.offer.interestRate }} % ročně</div>
       </div>
@@ -13,7 +13,7 @@
         <div class="text-gray">Měsíční splátka od</div>
         <div>{{ monthlyPaymentFormatted }}</div>
       </div>
-      <div class="flex fle-row justify-between">
+      <div class="flex flex-row justify-between">
         <div class="text-gray">RPSN</div>
         <div>{{ props.offer.apr }} %</div>
       </div>
