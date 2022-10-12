6cda40a49c077e0148f552621685ec59ad978385
Tomas Kundis <tomas.kundis@example.com>
2022-10-12 17:57:41 +0200

    FEAT: Add test play ground view and screen.

---
+++ example-for-endevel/src/views/TestPlaygroundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header />
+    <TestPlaygroundScreen />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Footer from '@/components/layout/Footer.vue';
+import Header from '@/components/layout/Header.vue';
+import TestPlaygroundScreen from '@/components/test-playground-screen/TestPlaygroundScreen.vue';
+</script>
+
+<style lang="scss" scoped></style>
