1af26ea4c0265e1abd83980781d05f48b97b50a1
Tomas Kundis <tomas.kundis@example.com>
2022-10-13 00:59:24 +0200

    FEAT: Add thousandSeparatorUs utility.

--- example-for-endevel/src/utils/thousandSeparator.ts
+++ example-for-endevel/src/utils/thousandSeparator.ts
@@ -3,3 +3,7 @@ import { envConfig } from '@/config/envConfig';
 export const thousandSeparator = (value: number) => {
   return value.toLocaleString(envConfig.locales.thousandSeparator);
 };
+
+export const thousandSeparatorUs = (value: number) => {
+  return value.toLocaleString('us-US');
+};
