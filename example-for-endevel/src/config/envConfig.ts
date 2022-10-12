f32014d99058c26f5c177351f8812c9f9ebec5bb
Tomas Kundis <tomas.kundis@example.com>
2022-10-13 00:48:26 +0200

    FEAT: Add locales to gloval config: thousandSeparator, currency.

--- example-for-endevel/src/config/envConfig.ts
+++ example-for-endevel/src/config/envConfig.ts
@@ -1,4 +1,8 @@
 export const envConfig = {
   appVersion: '0.0.6',
   githubUrl: 'https://github.com/tomas-kundis-developer/example-for-endevel',
+  locales: {
+    thousandSeparator: 'cs-CZ',
+    currency: 'CZK',
+  },
 };
