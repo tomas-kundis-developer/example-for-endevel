da6e09104258394ed0fa2ce91489f63b0f1522e4
Tomas Kundis <tomas.kundis@example.com>
2022-10-13 23:33:25 +0200

    FEAT: Add CommunicationError component.

---
+++ example-for-endevel/src/components/CommunicationError.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center font-semibold text-[#ff0000]">
+    <div>ERROR DURING COMMUNICATION WITH THE SERVER!</div>
+    <div class="mt-2">Please, try again.</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
