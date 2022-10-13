162530a39542663f5beceab4ddd134ab0b45f051
Tomas Kundis <tomas.kundis@example.com>
2022-10-13 03:52:55 +0200

    package.json: Increment version to 0.0.6-SNAPSHOT.

--- example-for-endevel/src/config/envConfig.ts
+++ example-for-endevel/src/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
-  appVersion: '0.0.5',
+  appVersion: '0.0.6-SNAPSHOT',
   githubUrl: 'https://github.com/tomas-kundis-developer/example-for-endevel',
   locales: {
     thousandSeparator: 'cs-CZ',
