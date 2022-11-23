b267aecbebfdaf42dad14356deb52d4e0e2b89a6
Tomas Kundis <tomas.kundis@example.com>
2022-11-23 02:22:50 +0100

    FEAT: Add information about connect. err. simulation.

--- example-for-endevel/src/components/CommunicationError.vue
+++ example-for-endevel/src/components/CommunicationError.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="text-center font-semibold text-red-500">
-    <div>ERROR DURING COMMUNICATION WITH THE SERVER!</div>
+    <div class="text-sm italic text-red-500 text-opacity-50">
+      Application simulates connection errors during fetching a bank offers!
+    </div>
+    <div class="mt-0">ERROR DURING COMMUNICATION WITH THE SERVER!</div>
     <div class="mt-2">Please, try again.</div>
   </div>
 </template>
